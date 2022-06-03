706c864a5474ebadf7248c21f3b03ed484556c85
JM-d3l4t0rr3 <72047760+JM-d3l4t0rr3@users.noreply.github.com>
2022-06-03 18:55:46 -0500

    Validando en el front

--- frontpqr/src/views/formularioPQR.jsx
+++ frontpqr/src/views/formularioPQR.jsx
@@ -1,10 +1,75 @@
 import React from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
 import NavBarTop from "../components/NavBarTop";
 
 class FormularioPQR extends React.Component{
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            nombre: '',
+            telefono: 0,
+            correo: '',
+            tipoPublicacion: '',
+            contenido: '',
+            validate: {
+                emailState: '',
+                nameState: '',
+                contenidoState: '',
+            },
+        };
+        this.handleChange = this.handleChange.bind(this);
+      }
     
+      handleChange = (event) => {
+        const { target } = event;
+        const value = target.value;
+        const { name } = target;
+    
+        this.setState({
+          [name]: value,
+        });
+      };
+
+    validateEmail(e) {
+        const emailRex =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+        const { validate } = this.state;
+    
+        if (emailRex.test(e.target.value)) {
+          validate.emailState = 'has-success';
+        } else {
+          validate.emailState = 'has-danger';
+        }
+    
+        this.setState({ validate });
+      }
+    
+      validateNombre(e) {
+        const { validate } = this.state;
+    
+        if (e.target.value !== "") {
+          validate.nameState = 'has-success';
+        } else {
+          validate.nameState = 'has-danger';
+        }
+    
+        this.setState({ validate });
+      }
+
+      validateSugerencia(e){
+        const { validate } = this.state;
+        
+        if (e.target.value !== "" && e.target.value.split(" ").length >= 10) {
+          validate.contenidoState = 'has-success';
+        } else {
+          validate.contenidoState = 'has-danger';
+        }
+    
+        this.setState({ validate });
+
+      }
+
     render(){
 
         return(
@@ -22,19 +87,43 @@ class FormularioPQR extends React.Component{
                             <Label>
                                 Nombre Completo
                             </Label>
-                            <Input />
+                            <Input name="nombre" 
+                            valid={this.state.validate.nameState === "has-success"}
+                            invalid={this.state.validate.nameState === "has-danger"}
+                            onChange={(e) => {
+                                this.handleChange(e)
+                                this.validateNombre(e)
+                            }}/>
+                            <FormFeedback>
+                                Por favor ingresa tu nombre
+                            </FormFeedback>
+                            <FormFeedback valid>
+                                Todo correcto con tu nombre :D
+                            </FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label>
                                Telefono o Celular
                             </Label>
-                            <Input  type="number" />
+                            <Input  name="telefono" type="number" onChange={(e) => {this.handleChange(e)}}/>
                         </FormGroup>
                         <FormGroup>
                             <Label>
                                 Correo electronico
                             </Label>
-                            <Input />
+                            <Input name="correo" 
+                            valid={this.state.validate.emailState === "has-success"}
+                            invalid={this.state.validate.emailState === "has-danger"}
+                            onChange={(e) => {
+                                this.handleChange(e)
+                                this.validateEmail(e)
+                            }}/>
+                            <FormFeedback>
+                                Parece que el correo que ingresaste no es valido
+                            </FormFeedback>
+                            <FormFeedback valid>
+                                Todo correcto con tu correo  :D
+                            </FormFeedback>
                         </FormGroup>
                         <FormGroup tag="fieldset">
                             <legend>
@@ -62,9 +151,24 @@ class FormularioPQR extends React.Component{
                             <Label>
                                 Escriba sus sugerencias o comentarios
                             </Label>
-                            <Input type="textarea" />
+                            <Input type="textarea"  name="contenido" 
+                            valid={this.state.validate.contenidoState === "has-success"}
+                            invalid={this.state.validate.contenidoState === "has-danger"}
+                            onChange={(e) => {
+                                this.handleChange(e)
+                                this.validateSugerencia(e)
+                            }}/>
+                            <FormFeedback>
+                                Tu peticion es muy breve, por favor se mas especifico 
+                            </FormFeedback>
+                            <FormFeedback valid>
+                                Todo correcto con tu peticion
+                            </FormFeedback>
+                            
                         </FormGroup>
-                        <Button color="warning" size = "lg">
+                        <Button color="warning" size = "lg" onClick={() => {
+                            console.log(this.state.nombre, this.state.telefono, this.state.correo, this.state.tipoPublicacion, this.state.contenido)
+                            }}>
                             Enviar
                         </Button>
                     </Form>
