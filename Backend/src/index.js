07f6ca9b94b7498c8de20c1d2aa610510b5bd529
JM-d3l4t0rr3 <72047760+JM-d3l4t0rr3@users.noreply.github.com>
2022-06-03 12:45:26 -0500

    Peticion Post completa

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,30 +14,37 @@ app.get("/", (request, response) => {
     response.send('<h1>Hola Mundo</h1> <br> <h3>Para acceder a la api usa la siguiente ruta <i> /api/pqr </i> </h3>')
 })
 
-app.get("/firepqr", async(request, response) => {
+app.get("/api/pqr", async(request, response) => {
     const result = await db.collection('pqr').get()
-    const pqrs = result.docs.map(doc => ({
-        id: doc.id,
-        contenido: doc.data().contenido,
-        correo: doc.data().correo,
-        fecha: doc.data().fecha,
-        nombre: doc.data().nombre,
-        telefono: doc.data().telefono,
-        tipoPublicacion: doc.data().tipoPublicacion
-    }))
-    console.log(pqrs)
-    response.json(pqrs)
-    //response.send('<h1>Conectando Firebase</h1>')
+    if (result.empty){
+        response.status(204).send('No hay ninguna queja ni sugerencia')
+    }else{
+        const pqrs = result.docs.map(doc => ({
+            id: doc.id,
+            nombre: doc.data().nombre,
+            telefono: doc.data().telefono,
+            correo: doc.data().correo,
+            tipoPublicacion: doc.data().tipoPublicacion,
+            contenido: doc.data().contenido,
+            fecha: doc.data().fecha,
+        }))
+        response.json(pqrs)
+    }
+    
 })
 
-app.get("/api/pqr", (request, response) => {
-    response.json(notes)
-})
 
-app.post("/api/pqr", (request, response) => {
-    const note = request.body
-    console.log(note)
-    response.json(note)
+app.post("/api/pqr", async(request, response) => {
+    try{
+        let pqr = request.body
+        let fechaAct = Date.now()
+        pqr = {...pqr, 
+        fecha: fechaAct}
+        await db.collection('pqr').add(pqr)
+        response.json(pqr)
+    }catch(error){
+        response.status(400).send(error.message)
+    }
 })
 
 const PORT = 3001
