4ca44709a3bcaebd7d447564481185eaa0c5c2af
JM-d3l4t0rr3 <72047760+JM-d3l4t0rr3@users.noreply.github.com>
2022-06-03 17:53:52 -0500

    Creacion del Formulario

--- frontpqr/src/views/formularioPQR.jsx
+++ frontpqr/src/views/formularioPQR.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import NavBarTop from "../components/NavBarTop";
 
 class FormularioPQR extends React.Component{
@@ -14,8 +15,59 @@ class FormularioPQR extends React.Component{
 
 
                 <div className="insideBox" >
-                    <div class ="shadow-lg p-1 rounded m-3 h-100">
-                    <h1>Hola este es el formulario</h1>
+                    <div class ="shadow-lg p-3 rounded m-3 h-100">
+                    <h1>PQR</h1>
+                    <Form>
+                        <FormGroup>
+                            <Label>
+                                Nombre Completo
+                            </Label>
+                            <Input />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>
+                               Telefono o Celular
+                            </Label>
+                            <Input  type="number" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>
+                                Correo electronico
+                            </Label>
+                            <Input />
+                        </FormGroup>
+                        <FormGroup tag="fieldset">
+                            <legend>
+                            Radio Buttons
+                            </legend>
+                            <FormGroup check>
+                            <Input
+                                name="radio1"
+                                type="radio"
+                            />
+                            {' '}
+                            <Label check>
+                                Queja
+                            </Label>
+                            </FormGroup>
+                            <FormGroup check>
+                            <Input name="radio1" type="radio"/>
+                            {' '}
+                            <Label check>
+                                Sugerencia
+                            </Label>
+                            </FormGroup>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>
+                                Escriba sus sugerencias o comentarios
+                            </Label>
+                            <Input type="textarea" />
+                        </FormGroup>
+                        <Button color="warning" size = "lg">
+                            Enviar
+                        </Button>
+                    </Form>
                     
                     </div>
                 </div>
--- frontpqr/src/views/verPQR.jsx
+++ frontpqr/src/views/verPQR.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBarTop from "../components/NavBarTop";
 import { APIGetPQRS } from '../apiServices/apiPQRS';
 import "./verPQR.css";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 
 class VerPQR extends React.Component{
 
@@ -9,12 +10,25 @@ class VerPQR extends React.Component{
         super(props);
     
         this.state = {
-          pqrs: []
+          pqrs: [],
+          intervalId: 0,
+          modal: false,
+          actualPQRS: [],
         };
     
-        
+        this.toggle = this.toggle.bind(this);
       }
+    toggle() {
+        
+        this.setState({modal: !(this.state.modal)});
+    }
+    
+    cargarDatos = (lista) =>{
+        this.toggle()
+        this.setState({actualPQRS: lista})
 
+
+    }
     getAllPQRS = async() =>{
 
         let PQall = await APIGetPQRS()
@@ -23,8 +37,29 @@ class VerPQR extends React.Component{
 
     componentDidMount(){
         this.getAllPQRS()
+        const idInt = setInterval(()=>{
+            this.getAllPQRS()
+        }, 5000)
         
+        this.setState({intervalId: idInt})
     }
+    componentWillUnmount(){
+
+        clearInterval(this.state.intervalId)
+    }
+
+    timeConverter = (UNIX_timestamp) =>{
+        var a = new Date(UNIX_timestamp);
+        var months = ['Enero','Febrero','Marzo','April','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre '];
+        var year = a.getUTCFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+       
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min  ;
+        return time;
+      }
 
     render(){
 
@@ -36,11 +71,75 @@ class VerPQR extends React.Component{
 
 
                 <div className="insideBox" >
-                    <div class ="shadow-lg p-1 rounded m-3 h-100">
-                    <h1>Hola este es verPQR</h1>
-                    <p class = "text-wrap">{JSON.stringify( this.state.pqrs )}</p>
+                    <div class ="shadow-lg p-3 rounded m-3 h-100">
+                    <h1>Acontinuacion tienes todos los usuarios</h1>
+                    <div className="scroller">
+                            
+                            {this.state.pqrs.map((it) => (
+                                <div class = "shadow-lg rounded-4 m-3 p-1  d-flex flex-wrap gap-3 " key={it.id}>
+                                    <div class = "border-end border-warning pe-3 w-25">
+                                        <h6>Fecha: </h6>
+                                        <p>{this.timeConverter(it.fecha)}</p>
+                                    </div>
+                                    <div class = "border-end border-warning pe-3 w-25 ">
+                                        <h6>Nombre: </h6>
+                                        <p class = "text-wrap">{it.nombre}</p>
+                                    </div>
+                                    <div class = "border-end border-warning pe-3 w-25">
+                                        <h6>Tipo Publicacion: </h6>
+                                        <p>{it.tipoPublicacion}</p>
+                                    </div>
+                                    <div  class="m-2 align-self-end">
+                                        <Button color="primary" size = "lg" outline onClick={() => this.cargarDatos([it.nombre,it.telefono,it.correo, it.tipoPublicacion,it.contenido, it.fecha])}>
+                                            Ver Mas
+                                        </Button>
+                                    </div>
+
+                                    
+                                </div>
+                            ))}
+                        
+                    </div>
+                    
                     </div>
                 </div>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} >
+                    <ModalHeader>
+                        Informacion del Egresado
+                    </ModalHeader>
+                    <ModalBody>
+                        <div class="vstack gap-3">
+                            <div >
+                                <h4>Nombre:</h4>
+                                <h6>{this.state.actualPQRS[0]}</h6>
+                            </div>
+                            <div >
+                                <h4>Telefono:</h4>
+                                <h6>{this.state.actualPQRS[1]}</h6>
+                            </div>
+                            <div >
+                                <h4>Correo:</h4>
+                                <h6>{this.state.actualPQRS[2]}</h6>
+                            </div>
+                            
+                            <div >
+                                <h4>Tipo de Publicacion:</h4>
+                                <h6>{this.state.actualPQRS[3]}</h6>
+                            </div>    
+                            <div >
+                                <h4>Contenido:</h4>
+                                <div class= "text-wrap">
+                                    {this.state.actualPQRS[4]}
+                                </div>
+                                
+                            </div>
+                        </div>
+                                
+                    </ModalBody>
+                    <ModalFooter>
+                        Fecha: {this.timeConverter(this.state.actualPQRS[5])}
+                    </ModalFooter>
+                </Modal>
                 
             </div>
 
